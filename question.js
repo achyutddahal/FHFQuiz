43c2675f2ef020b73e3c474ff4b56842ce5eea09
Naresh Maharjan <nareshmaharjan@lftechnology.com>
2015-04-01 17:00:10 +0545

    inital commit

---
+++ question.js
@@ -0,0 +1,12 @@
+var question = {
+	"1":"whats yout name",
+	"2" : "test2",
+	"3" : "test3",
+	"4" : "Question 4",
+	"5" : "test5",
+	"6" : "test6",
+	"7" : "test7",
+	"8" : "Question 8",
+	"9" : "Question 9",
+	"10" : "Question 10"
+}
